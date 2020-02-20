0f4d48792320f04b072e1c038b0bc45a2c5d75bc
Jamie Gallen <jmegallen@gmail.com>
2020-02-20 16:21:41 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
-import github from '../img/github-icon.svg'
+import github from '../img/social/github-icon.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
